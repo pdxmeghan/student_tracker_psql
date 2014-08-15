aabd2f73d6ead776fd70f24669b57a82dec25c58
Meghan <pdxmeghan@gmail.com>
2014-08-15 14:35:55 -0700

    adds the ability to save each course, and list out all the courses."

--- lib/course.rb
+++ lib/course.rb
@@ -6,4 +6,22 @@ class Course
     @course_number = attributes['course_number']
     @id = attributes['id'].to_i
   end
+
+  def self.all
+    courses = []
+    results = DB.exec("SELECT * FROM courses;")
+    results.each do |result|
+      courses << Course.new(result)
+    end
+    courses
+  end
+
+  def save
+    results = DB.exec("INSERT INTO courses (name, course_number) VALUES ('#{@name}', '#{@course_number}') RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
+  def ==(another_course)
+    @name == another_course.name && @course_number == another_course.course_number
+  end
 end
--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -12,5 +12,24 @@ describe Course do
     expect(test_course.course_number).to eq 'CK306'
   end
 
+  it 'will start out with an empty array' do
+    expect(Course.all).to eq []
+  end
+
+  it 'will save a new course to the database' do
+    test_course = Course.new({'name' => 'Cooking Shrimp', 'course_number' => 'CK306'})
+    test_course.save
+    expect(Course.all).to eq [test_course]
+  end
+
+  it 'will be the same course if it has the same name and course number' do
+    test_course = Course.new({'name' => 'Cooking Shrimp', 'course_number' => 'CK306'})
+    test_course.save
+    test_course1 = Course.new({'name' => 'Cooking Shrimp', 'course_number' => 'CK306'})
+    test_course1.save
+    expect(test_course1).to eq test_course
+  end
+
+
 
 end
